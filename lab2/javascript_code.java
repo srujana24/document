98e5b4268fac9abe8e20a3caaa4302fee6e38a4f
srujana24 <skkd2@mail.umkc.edu>
2014-02-04 23:59:35 -0600

    lab2
    web client application

---
+++ lab2/javascript_code.java
@@ -0,0 +1,38 @@
+var display;
+var directionsService = new google.maps.DirectionsService();     
+var map2;
+function initialize()
+{
+            display = new google.maps.DirectionsRenderer();        
+    var center = new google.maps.LatLng(0, 0);    
+    var myOptions =
+    {
+            zoom:7,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            center: center
+    }
+    map2 = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    display.setMap(map2);
+    var start = "Kansas City";    
+    var end = "New York";  
+    var request =
+    {
+            origin:start,
+            destination:end,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING          
+    };
+    directionsService.route(request, function(response, status)
+    {
+            if (status == google.maps.DirectionsStatus.OK)             {
+            display.setDirections(response);         
+            }
+    });
+}
+
+var button = document.getElementById('button');
+
+var onClick = function() {
+    initialize();
+};
+
+button.addEventListener('click', onClick, false);
